b45681795550c35edbb3b7b89ffe3d209b8ba4c8
zhanghtt <hsnn@hsnndeMacBook-Air.local>
2017-05-08 00:11:46 +0800

    网站公告

---
+++ js/ajax.js
@@ -0,0 +1,173 @@
+var download_path= "_downloads/ylzz/";
+//var host= 'http://192.168.1.108:8899'
+//var host = "http://192.168.1.108:1717";
+//var host = "http://hsnn12.3322.org:8053";
+var host = 'http://gyl.elian.net'
+//var host = 'http://192.168.1.2:9003'
+//var host = "http://hzyl12.8800.org:9003"
+//var host = 'http://www.elianxinxi.com:8101/'
+//var host ='http://192.168.1.152:8899'
+ //var host = "http://www.elianxinxi.com"
+//var host  =   "http://hsnn12.3322.org:8052/"
+//var host = "http://hsnn12.3322.org:8053"
+
+var ajax_getCode = host + "/api/common/ValidateCode";
+var ajax_registerURL = host + "/api/common/register";
+var ajax_SearchURL = host + "/api/common/FullSearch";
+var ajax_indexSlideURL = host + "/api/common/IndexSlide";//轮播图
+
+var ajax_loginURL = host + "/api/common/Login";
+var ajax_resetPwdURL = host + "/api/common/ResetPwd";
+var ajax_menberInfoURL = host + "/api/common/Member";//个人信息
+var ajax_modMemberURL = host + "/api/common/ModMember";//修改个人信息；
+var ajax_modPwordURL = host + "/api/common/ModPword";
+
+var ajax_addCollectionURl = host + "/api/common/AddCollection";
+var ajax_collectionListURL = host + "/api/common/Collection";
+var ajax_cancelCollectionURl = host + "/api/common/CancelCollection";
+
+
+var ajax_uploadHeadImgURl = host + "/api/common/UpLoadHeadImages";//头像上传
+
+var ajax_msgListURl = host + "/api/common/MsgList";//消息列表
+var ajax_msgDetailURl = host + "/api/common/MsgDetail";//消息详情
+var ajax_msgDelURL = host + "/api/common/MsgDel";//删除消息
+
+var ajax_articleDetailURL = host + "/api/common/ArticleDetail";//文章详情url
+
+var ajax_addtimePlanURl = host + "/api/common/AddTimePlan";//添加时间计划
+var ajax_timePlaneURL = host + "/api/common/TimePlan";//时间列表
+var ajax_cacelTimeWarnURL = host + "/api/common/CancelTimePlanWarm";//时间不在提醒
+
+var ajax_updateTimeWarnURL = host + "/api/common/UpdateTimePlanWarm";//更新时间继续提醒
+
+var ajax_cacelTimeURl = host + "/api/common/CancelTimePlan";//删除修改时间
+
+var ajax_loginoutURL = host + "/api/common/logout";//退出登陆接口
+
+var ajax_pushURL = host + "/api/common/AppSetting";//推送设置
+
+var ajax_update_appVersion = '/api/common/CheckUpdate' 
+
+//获取公司信息
+var ajax_getAboutCompany = host + '/api/common/GetCompanyInfo'
+
+function getNowFormatDate() {
+		var date = new Date();
+		var month = date.getMonth() + 1;
+		var strDate = date.getDate();
+		if (month >= 1 && month <= 9) {
+		    month = "0" + month;
+		}
+		if (strDate >= 0 && strDate <= 9) {
+		    strDate = "0" + strDate;
+		}
+		var strHours = date.getHours()
+		if (strHours >= 0 && strHours <= 9) {
+			strHours = "0" + strHours;
+		}
+		var strMinutes = date.getMinutes();
+		if (strMinutes >=0 && strMinutes <=9){
+			strMinutes = "0" + strMinutes
+		}
+		var strSeconds = date.getSeconds();
+		if (strSeconds >=0 && strSeconds <=9){
+			strSeconds = "0" + strSeconds
+		}
+		var currentdate = date.getFullYear() +""+ month +""+ strDate +""+ strHours+""+ strMinutes+""+ strSeconds;
+		date = null,month = null,strDate=null,strHours=null,strMinutes=null,strSeconds=null;
+		return currentdate;
+	};
+
+
+
+
+function openpage(url){
+	//新建windows页面，传递url值过去并在页面中打开
+	     mui.openWindow({
+					id: url,
+					url: 'windows.html',
+					styles: {
+						top: 0,
+						bottom: 0,
+						bounce: 'none',
+    					popGesture: 'close' // 'hide', 'close','none'
+					},
+					 extras:{
+					 	pageurl:url
+					 },
+					 show:{
+					      autoShow:true,//页面loaded事件发生后自动显示，默认为true
+					      aniShow:'auto',//页面显示动画，默认为”slide-in-right“；
+					      duration:300//页面动画持续时间，Android平台默认100毫秒，iOS平台默认200毫秒；
+					    },
+					 waiting:{
+					 	autoShow:false,
+					 },
+					createNew:false,//是否重复创建同样id的webview，默认为false:不重复创建，直接显示
+				});
+};
+
+function openpageIndex(url){
+	//新建windows页面，传递url值过去并在页面中打开
+	    mui.openWindow({
+					id: url,
+					url: 'pages/windows.html',
+					styles: {
+						top: 0,
+						bottom: 0,
+						bounce: 'none',
+    					popGesture: 'close' // 'hide', 'close','none'
+					},
+					 extras:{
+					 	pageurl:url,
+					 },
+					  show:{
+					      autoShow:true,//页面loaded事件发生后自动显示，默认为true
+					      aniShow:'auto',//页面显示动画，默认为”slide-in-right“；
+					      duration:300//页面动画持续时间，Android平台默认100毫秒，iOS平台默认200毫秒；
+					    },
+					 waiting:{
+					 	autoShow:false,
+					 },
+					createNew:false,//是否重复创建同样id的webview，默认为false:不重复创建，直接显示
+				});
+};
+
+//判断用户是否登录，未登录直接转到登录页面
+function judgeLogin(){
+	var userdata=plus.storage.getItem('memberdata');
+	if(userdata!=''&&userdata!=null){
+		var $user =  JSON.parse(userdata);
+		if(($user.token == null)||$user.token == ""){
+			return false;
+		}
+		return $user;
+	}
+	return false;
+};
+//防止连续点击导致webView打开失败
+var tap_first = null;
+function unsafe_tap() {
+	if (!tap_first) {
+		tap_first = new Date().getTime();
+		setTimeout(function() {
+			tap_first = null;
+		},1500)
+	}else {
+		return true;
+	}
+}
+
+
+ String.prototype.endWith=function(str){  
+	            if(str==null||str==""||this.length==0||str.length>this.length)  
+	              return false;  
+	            if(this.substring(this.length-str.length)==str)  
+	              return true;  
+	            else  
+	              return false;  
+	            return true;  
+	        }  
+
+
---
+++ js/g/webnoticeUtils.js
@@ -0,0 +1,133 @@
+var GUtils = function(mui) {
+    var me = this;
+    var code = "0", info = "", c = "1", catesList, curCateIndex, ChannelName;
+    me.setCurItem = function(i) {
+        me.curCateIndex = i;
+        me.c = me.catesList[i]["catid"];
+        if (i == 0) {
+        		me.ChannelName = "";
+        }else {
+        		me.ChannelName = me.catesList[i]["catname"];
+        }
+    }, me.getCurCateInfo = function() {
+        return me.catesList[me.curCateIndex];
+    }, me.setCurCateId = function(c) {
+        me.c = c;
+    }, me.getCurCateId = function() {
+        return me.c;
+    }, me.getMenuCatesInfo = function() {
+        me.code = statusCode.SUCCESS;
+        me.catesList = [{
+            catid: 1,
+            catname: "全部"
+        }, {
+            catid: 2,
+            catname: "招标项目"
+        }, {
+            catid: 3,
+            catname: "招标资讯"
+        }, {
+            catid: 4,
+            catname: "基药政策"
+        }, {
+            catid: 5,
+            catname: "新农合政策"
+        }, {
+            catid: 6,
+            catname: "医保政策"
+        }, {
+            catid: 7,
+            catname: "低价药政策"
+        }, {
+            catid: 8,
+            catname: "OTC政策"
+        }, {
+            catid: 9,
+            catname: "其他政策"
+        }];
+        me.info = me.catesList;
+        return me.getResult();
+    }, me.getImgNewsByCid = function(url, params,callback,curDiv,t) {
+        try {
+           mui.ajax({
+                type: "POST",
+                data: {
+                    transtime: params
+                },
+                async: false,
+                url: url,
+                dataType: "json",
+                success: function(data_) {
+                    data = JSON.parse(data_);
+                    if (data.result == 1) {
+                        me.code = statusCode.SUCCESS;
+                        me.info = data.Slide;
+                    } else {
+                        me.code = statusCode.NODATA;
+                    }
+                    callback(me,curDiv,t)
+                },
+                error: function() {
+                    me.code = statusCode.ERROR;
+                }
+            });
+        } catch (e) {
+            me.code = statusCode.ERROR;
+        }
+        //return me.getResult();
+    }, me.getNewsListInfo = function(url, atime, Pageindex,loadListCallback,curDiv,page) {
+    	var user = null,token='';
+    		user = judgeLogin();
+    		 if(user){
+    		 	 token = user.token;
+    		 }else{
+    		 	token='';
+    		 }
+   	var data_form = {
+                    transtime: atime,
+                    keyword: "",
+                    ChannelName: me.ChannelName,
+                    AreaID: "",
+                    Pageindex: Pageindex,
+                    Type: 3,
+                    token:token
+             };
+           mui.ajax({
+                type: "POST",
+                url: url,
+                async: true,
+                data: data_form,
+                dataType: "json",
+                success: function(data_) {
+//              	console.log(url)
+//              	console.log(JSON.stringify(data_form))
+              	console.log(data_)
+                    data = JSON.parse(data_);
+                    if (data.result == 1 && data.SearchResult.length != 0) {
+                        me.code = statusCode.SUCCESS;
+                        me.info = data.SearchResult;
+                    } else {
+                        me.code = statusCode.NODATA;
+                        plus.nativeUI.toast("没有更多数据了")
+                    }
+                    loadListCallback(me,curDiv,page)
+                },
+                error: function() {
+                	   plus.nativeUI.toast("网络超时")
+                	   plus.nativeUI.closeWaiting();
+                    me.code = statusCode.ERROR;
+                }
+            });
+       
+        //return me.getResult();
+    }, me.getResult = function() {
+        var o = {};
+        if (me.code == "undefined" || me.code == "0") {
+            o.reCode = me.statusCode.ERROR;
+        } else {
+            o.reCode = me.code;
+            o.reInfo = me.info;
+        }
+        return o;
+    };
+};
